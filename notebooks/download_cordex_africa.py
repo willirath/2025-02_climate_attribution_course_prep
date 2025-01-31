5816a4886cab76db5ceb75d28c95d4f41bc05653
Willi Rath <wrath@geomar.de>
2025-01-31 20:24:48 +0000

    Finalize download scripts

---
+++ notebooks/download_cordex_africa.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import papermill
+
+params = pd.read_csv("parameters.csv", delim_whitespace=True)
+params = params.where(params.domain == "africa").dropna()
+
+for n in range(len(params)):
+    pdict = dict(params.iloc[n])
+    papermill.execute_notebook(
+        "download_cordex.ipynb",
+        "download_cordex.{domain}_{horizontal_resolution}_{gcm_model}_{rcm_model}.ipynb".format(**pdict),
+        parameters=pdict,
+    )
